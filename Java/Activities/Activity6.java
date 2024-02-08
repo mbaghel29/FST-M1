6c10667e00c3d9820d5ac1af619d4cdc63c9f994
mbaghel29 <154311821+mbaghel29@users.noreply.github.com>
2024-02-08 20:33:26 +0530

    Add files via upload

---
+++ Java/Activities/Activity6.java
@@ -0,0 +1,23 @@
+package activities;
+
+public class Activity6 {
+    public static void main(String[] args) throws InterruptedException {
+
+        Plane plane = new Plane(10);
+
+        plane.onboard("Milan");
+        plane.onboard("Ketaki");
+        plane.onboard("Kelan");
+
+        System.out.println("Plane took off at: " + plane.takeOff());
+
+        System.out.println("People on the plane: " + plane.getPassengers());
+
+        Thread.sleep(5000);
+
+        plane.land();
+
+        System.out.println("Plane landed at: " + plane.getLastTimeLanded());
+        System.out.println("People on the plane after landing: " + plane.getPassengers());
+    }
+}
